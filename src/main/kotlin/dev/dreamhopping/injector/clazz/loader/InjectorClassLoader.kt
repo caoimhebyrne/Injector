9df13f2ec6441f270637cb9d77c04a70a4bf2372
dreamhopping <71222289+dreamhopping@users.noreply.github.com>
2021-03-26 11:51:06 +0000

    Add license to source files

--- src/main/kotlin/dev/dreamhopping/injector/clazz/loader/InjectorClassLoader.kt
+++ src/main/kotlin/dev/dreamhopping/injector/clazz/loader/InjectorClassLoader.kt
@@ -1,3 +1,21 @@
+/*
+ *     Injector is a runtime class modification library for Kotlin
+ *     Copyright (C) 2021  Conor Byrne
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package dev.dreamhopping.injector.clazz.loader
 
 import dev.dreamhopping.injector.clazz.transformer.impl.InjectorClassTransformer
